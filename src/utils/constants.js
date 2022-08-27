bb6dfa61f3c05f294a78d4bdd6204c35d8f7a528
Diego-Sant <diego_santana@unigranrio.br>
2022-08-26 23:42:15 -0300

    finalcommit

---
+++ src/utils/constants.js
@@ -0,0 +1,35 @@
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import VideoCameraFrontIcon from "@mui/icons-material/VideoCameraFront";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import TvIcon from "@mui/icons-material/Tv";
+
+export const logo = "https://i.postimg.cc/3NqK1023/logo.png";
+
+export const categories = [
+  { name: "Flamengo", icon: <SportsSoccerIcon /> },
+  { name: "Lyrics Song", icon: <LibraryMusicOutlinedIcon /> },
+  { name: "Kimetsu no Yaiba", icon: <TvIcon /> },
+  { name: "Boku no Hero", icon: <TvIcon /> },
+  { name: "Shingeki no Kyojin", icon: <TvIcon /> },
+  { name: "JavaScript Mastery", icon: <VideoCameraFrontIcon /> },
+  { name: "Lives do Cellbit", icon: <VideoCameraFrontIcon /> },
+  { name: "Lives do alanzoka", icon: <VideoCameraFrontIcon /> },
+  { name: "Felca", icon: <VideoCameraFrontIcon /> },
+  { name: "orochidois", icon: <VideoCameraFrontIcon /> },
+  { name: "Lemuria TFT", icon: <VideoCameraFrontIcon /> },
+  { name: "Shawn TFT", icon: <VideoCameraFrontIcon /> },
+  { name: "BunnyFuFuu TFT", icon: <VideoCameraFrontIcon /> },
+  { name: "GEMAPLYS", icon: <VideoCameraFrontIcon /> },
+  { name: "viniccius13", icon: <VideoCameraFrontIcon /> },
+  { name: "Corbucci Eats", icon: <VideoCameraFrontIcon /> },
+  { name: "ToastedShoes", icon: <VideoCameraFrontIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "JavaScript Mastery";
+export const demoVideoTitle =
+  "Vá ver Sandman(Esse vídeo não funciona não clique)";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
